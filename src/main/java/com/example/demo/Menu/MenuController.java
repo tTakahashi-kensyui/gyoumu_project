e592061ae5d7cac9b41c44760fa17f001614d5d5
t-takahashi <you@example.com>
2025-06-25 14:55:56 +0900

    Entity,Daoクラス,index.htmlを作成し、トップページの表示まで

--- src/main/java/com/example/demo/Menu/MenuController.java
+++ src/main/java/com/example/demo/Menu/MenuController.java
@@ -1,5 +1,24 @@
 package com.example.demo.Menu;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.MenuDao;
+
+@Controller
 public class MenuController {
+
+	private final MenuDao menuDao;
+	
+	public MenuController(MenuDao menuDao) {
+		this.menuDao = menuDao;
+	}
+
+	@RequestMapping("/")
+	public String topPage(Model model) {
+		model.addAttribute("title", "トップページ");
+		return "Menu/index";
+	}
 
 }
